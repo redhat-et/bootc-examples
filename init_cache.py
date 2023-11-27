6c8d991f6aa120bd5b720945c4e569836fd8089a
Sally O'Malley <somalley@redhat.com>
2023-11-27 12:28:01 -0500

    remove extra files

--- init_cache.py
+++
@@ -1,6 +0,0 @@
-# cache_init.py
-from transformers import AutoModel, AutoTokenizer
-
-model_name = "lmsys/fastchat-t5-3b-v1.0"
-AutoTokenizer.from_pretrained(model_name)
-AutoModel.from_pretrained(model_name)
